51ed96de66cf24cc44729e920a4b540e5ea482fc
Davut Uçar <davutucar@gmail.com>
2018-04-09 17:58:33 +0300

    Update PickUser.java

--- src/simulation/PickUser.java
+++ src/simulation/PickUser.java
@@ -1,4 +1,20 @@
 package simulation;
 
+import user.OnlineFriendsAndStatus;
+import user.TimeBasedInformation;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PickUser {
+    public long findDelegation(TimeBasedInformation timeBasedInformation) {
+        long delegated;
+        int sizeOnlineFriend = timeBasedInformation.getOnlineFriendsCount();
+        int randomIndexOfUser = ThreadLocalRandom.current().nextInt(0, sizeOnlineFriend + 1);
+
+        delegated = timeBasedInformation.getOnlineFriendsList().get(randomIndexOfUser).getFriendUserID();
+
+        System.out.println("picked index = " + randomIndexOfUser + ". Delegated userid = " + delegated);
+        
+        return delegated;
+    }
 }
