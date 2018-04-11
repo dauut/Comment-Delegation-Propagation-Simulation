546f71a627a7d5797c63cb799c79df195c98a967
Davut Uçar <davutucar@gmail.com>
2018-04-11 16:06:55 +0300

    main algorithm

--- src/io/Main.java
+++ src/io/Main.java
@@ -1,7 +1,11 @@
 package io;
 
 import constants.Constants;
-import test.ReadOneFile;
+import org.joda.time.DateTime;
+import simulation.PickUser;
+import user.DelegationInfo;
+import user.DelegationTimeAndUsers;
+import user.OnlineFriendsAndStatus;
 import user.UserInformations;
 
 import java.text.ParseException;
@@ -10,80 +14,79 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        ArrayList<UserInformations> usersList = new ArrayList<>();
-//        ReadFiles getUserFromData = new ReadFiles();
-//        usersList = getUserFromData.getUserList();
+        ArrayList<UserInformations> usersList;
+        ReadFiles getUserFromData = new ReadFiles();
+        DelegationInfo delegationInfo = new DelegationInfo(); // it may be arraylist in the future
+//        ArrayList<DelegationTimeAndUsers> delegationTimeAndUsersArrayList = new ArrayList<DelegationTimeAndUsers>();
+        ArrayList<Long> delegatedUserIDList = new ArrayList<>();
+        ArrayList<Date> delegationTimeList = new ArrayList<>();
 
-        ReadOneFile readUsersFromOneFile = new ReadOneFile();
-        usersList = readUsersFromOneFile.getUserList();
-        for (int i = 0; i < usersList.get(0).getUserActivites().size(); i++)
-            System.out.println(usersList.get(0).getUserActivites().get(i).getCurrentTimestamp());
+        boolean isUserOffline = false;
 
-
-
-
-        /*OnlineFriendsAndStatus onlineFriendsAndStatus1 = new OnlineFriendsAndStatus();
-        onlineFriendsAndStatus1.setFriendUserID(1);
-        onlineFriendsAndStatus1.setDeviceType("IOS");
-        onlineFriendsAndStatus1.setStatus("idle");
-
-        OnlineFriendsAndStatus onlineFriendsAndStatus2 = new OnlineFriendsAndStatus();
-        onlineFriendsAndStatus2.setFriendUserID(2);
-        onlineFriendsAndStatus2.setDeviceType("Android");
-        onlineFriendsAndStatus2.setStatus("online");
-
-        OnlineFriendsAndStatus onlineFriendsAndStatus3 = new OnlineFriendsAndStatus();
-        onlineFriendsAndStatus3.setFriendUserID(3);
-        onlineFriendsAndStatus3.setDeviceType("Android");
-        onlineFriendsAndStatus3.setStatus("idle");
-
-        ArrayList<OnlineFriendsAndStatus> onlineFriendsAndStatusArrayList = new ArrayList<>();
-        onlineFriendsAndStatusArrayList.add(onlineFriendsAndStatus1);
-        onlineFriendsAndStatusArrayList.add(onlineFriendsAndStatus2);
-        onlineFriendsAndStatusArrayList.add(onlineFriendsAndStatus3);
-
-        TimeBasedInformation timeBasedInformations = new TimeBasedInformation();
-        timeBasedInformations.setActiveFriendsCount(1);
-        String theDate = "05/20/2014 01:29:32 am";
-        SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss a", Locale.US);
-        try {
-            Date date = format.parse(theDate);
-            timeBasedInformations.setCurrentTimestamp(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        timeBasedInformations.setIdleFriendsCount(2);
-        timeBasedInformations.setMobileActiveCount(1);
-        timeBasedInformations.setMobileIdleCount(0);
-        timeBasedInformations.setWebUsersCount(0);
-        timeBasedInformations.setOnlineFriendsList(onlineFriendsAndStatusArrayList);
-
-        TimeBasedInformation timeBasedInformations2 = new TimeBasedInformation();
-        timeBasedInformations.setActiveFriendsCount(1);
-        String theDate2 = "05/20/2014 02:29:32 am";
-        SimpleDateFormat format2 = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss a", Locale.US);
-        try {
-            Date date = format2.parse(theDate);
-            timeBasedInformations.setCurrentTimestamp(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        timeBasedInformations.setIdleFriendsCount(2);
-        timeBasedInformations.setMobileActiveCount(1);
-        timeBasedInformations.setMobileIdleCount(0);
-        timeBasedInformations.setWebUsersCount(0);
-        timeBasedInformations.setOnlineFriendsList(onlineFriendsAndStatusArrayList);
-
-        ArrayList<TimeBasedInformation> timeBasedInformationsArrayList = new ArrayList<>();
-        timeBasedInformationsArrayList.add(timeBasedInformations);
-        timeBasedInformationsArrayList.add(timeBasedInformations2);
-        UserInformations user1 = new UserInformations();
-        user1.setUserId(4);
-        user1.setUserActivites(timeBasedInformationsArrayList);
-
-        System.out.println(user1.getUserActivites().get(1));
+        //Date settings
+        /*
+        Date simulationStartDate = null;
+        Date simulationEndDate = null;
+        SimpleDateFormat simpleStartFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
+        simulationStartDate = simpleStartFormat.parse(Constants.getSimulationStartDate());
+        simulationEndDate = simpleStartFormat.parse(Constants.getSimulationEndDate());
+        DateTime simulationStart = new DateTime(simulationStartDate);
+        DateTime simulationEnd = new DateTime(simulationEndDate);
         */
+        long delegatedUserID;
+        usersList = getUserFromData.getUserList();
+/*
+        for (DateTime date = simulationStart; date.isBefore(simulationEnd); date = date.plusMinutes(1)) {
+            PickUser pickUser = new PickUser();
+            System.out.println(date.toString());
+
+        }
+*/
+        PickUser pickUser = new PickUser();
+        delegatedUserID = pickUser.findDelegation(usersList.get(0).getUserActivites().get(0));
+        delegationInfo.setUserId(usersList.get(0).getUserId());
+        int indexOfExistingDelegation;
+        for (int i = 0; i < 50/*usersList.get(0).getUserActivites().size()*/; i++) {
+            pickUser = new PickUser();
+            ArrayList<Long> onlineFriendUserList = new ArrayList<>();
+//            onlineFriendUserList = usersList.get(0).getUserActivites().get(i).
+
+            //really bad
+            for (int k = 0; k < usersList.get(0).getUserActivites().get(i).getOnlineFriendsList().size(); k++){
+                onlineFriendUserList.add(usersList.get(0).getUserActivites().get(i).getOnlineFriendsList().get(k).getFriendUserID());
+            }
+
+//            if (!delegatedUserIDList.contains(delegatedUserID)) {
+
+            //burasi duzeltilecek. burada contain atanmis listedeki elemanlar uzerinden yapilacak.
+            //metodize edilmesi gereken cok sey var
+            //kod karman corman oldu
+            if (!onlineFriendUserList.contains(delegatedUserID)) {
+                delegatedUserID = pickUser.findDelegation(usersList.get(0).getUserActivites().get(i));
+                delegatedUserIDList.add(delegatedUserID);
+                delegationTimeList.add(usersList.get(0).getUserActivites().get(i).getCurrentTimestamp());
+                delegationInfo.setChainDepth(delegatedUserIDList.size());
+            } else {
+                indexOfExistingDelegation = delegatedUserIDList.indexOf(delegatedUserID);
+                //lets delete after that user
+                for (int j = indexOfExistingDelegation + 1; j < delegatedUserIDList.size(); j++) {
+                    delegatedUserIDList.remove(delegatedUserIDList.get(j));
+                    delegationTimeList.remove(delegationTimeList.get(j));
+                }
+                delegationInfo.setChainDepth(delegatedUserIDList.size());
+            }
+        }
+
+
+        //delegatedUserID = pickUser.findDelegation(usersList.get(0).getUserActivites().get(0));
+
+
+//        ReadOneFile readUsersFromOneFile = new ReadOneFile();
+//        usersList = readUsersFromOneFile.getUserList();
+//        for (int i = 0; i < usersList.get(0).getUserActivites().size(); i++)
+//            System.out.println(usersList.get(0).getUserActivites().get(i).getCurrentTimestamp()+": " + i);
+
     }
 }
