44f5027d8829f3f5d6d8a4443ca1ce9bc5bcf4a8
Davut Uçar <davutucar@gmail.com>
2018-04-18 20:41:33 +0300

    update

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -4,12 +4,17 @@ public final class Constants {
 
     //    private static final String DATA_PATH = "C:\\Users\\dauut\\Desktop\\Facebook_online_status1";
     private static final String DATA_PATH = "C:\\Users\\DavutU\\Desktop\\test";
-    private static final String OUTPUT_PATH = "C:\\Users\\DavutU\\Desktop\\testout\\tmpres.txt";
+    private static final String OUTPUT_PATH = "C:\\Users\\DavutU\\Desktop\\testout\\simulation.txt";
+    private static final String GENERAL_INFO_OUTPUT_PATH = "C:\\Users\\DavutU\\Desktop\\testout\\simulationGeneralInfo.txt";
     private static final String SIMULATION_START_DATE = "05/28/2014 12:01:00 am";
     private static final String SIMULATION_END_DATE = "06/11/2014 11:59:00 Pm";
 
     public static String getOutputPath() {
         return OUTPUT_PATH;
+    }
+
+    public static String getGeneralInfoOutputPath() {
+        return GENERAL_INFO_OUTPUT_PATH;
     }
 
     public static String getSimulationStartDate() {
