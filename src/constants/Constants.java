eba9f42a159d8d54a903dcce7944a2121d7aaad5
Davut Uçar <davutucar@gmail.com>
2018-04-19 12:19:53 +0300

    update with generic path

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -3,23 +3,27 @@ package constants;
 public final class Constants {
 
     //work
-    /*private static final String DATA_PATH = "C:\\Users\\DavutU\\Desktop\\test";
-    private static final String OUTPUT_PATH = "C:\\Users\\DavutU\\Desktop\\testout\\simulation.txt";*/
+    private static final String DATA_PATH = "C:\\Users\\DavutU\\Desktop\\test";
+    private static final String OUTPUT_FOLDER_PATH = "C:\\Users\\DavutU\\Desktop\\testout";
+    private static final String OUTPUT_PATH = "C:\\Users\\DavutU\\Desktop\\testout\\simulation";
+    private static final String GENERAL_INFO_OUTPUT_PATH = "C:\\Users\\DavutU\\Desktop\\testout\\simulationGeneralInfo";
 
     //home
-    private static final String DATA_PATH = "C:\\Users\\dauut\\Desktop\\Facebook_online_status1";
     /*
-    private static final String OUTPUT_PATH = "C:\\Users\\dauut\\Desktop\\testout\\simulation.txt";
-    private static final String GENERAL_INFO_OUTPUT_PATH = "C:\\Users\\dauut\\Desktop\\testout\\simulationGeneralInfo.txt";
-    */
-
+    private static final String DATA_PATH = "C:\\Users\\dauut\\Desktop\\Facebook_online_status1";
     private static final String OUTPUT_PATH = "C:\\Users\\dauut\\Desktop\\testout\\simulation";
     private static final String GENERAL_INFO_OUTPUT_PATH = "C:\\Users\\dauut\\Desktop\\testout\\simulationGeneralInfo";
+    */
+
     private static final String SIMULATION_START_DATE = "05/28/2014 12:01:00 am";
     private static final String SIMULATION_END_DATE = "06/11/2014 11:59:00 Pm";
 
     public static String getOutputPath() {
         return OUTPUT_PATH;
+    }
+
+    public static String getOutputFolderPath() {
+        return OUTPUT_FOLDER_PATH;
     }
 
     public static String getGeneralInfoOutputPath() {
