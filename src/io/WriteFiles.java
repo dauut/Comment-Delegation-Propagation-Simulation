7e4c0488579752b57acdb0606569ad3332cb21ea
Davut Uçar <davutucar@gmail.com>
2018-05-02 16:11:28 +0300

    Add files via upload

--- src/io/WriteFiles.java
+++ src/io/WriteFiles.java
@@ -19,70 +19,10 @@ import java.util.Date;
  * */
 
 public class WriteFiles {
-    public void writeInfoFiles(ArrayList<Long> delegatedUserIdList, ArrayList<Date> delegationTimeList,
-                               ArrayList<Integer> chainList, String fileName, int theTimestampIndex,
-                               ArrayList<UserInformations> userList, int userIndex, int theTour, long friendUserID) {
-        TableBuilder tb = new TableBuilder();
-        File dir;
-        String dirPath = Constants.getOutputFolderPath() + "\\" + userList.get(userIndex).getUserId();
-        dir = new File(dirPath);
-        if (!dir.exists()) {
-            if (dir.mkdir()) {
-                System.out.println("Directory is created! " + userList.get(userIndex).getUserId());
-            } else {
-                System.out.println("Failed to create directory! " + userList.get(userIndex).getUserId());
-            }
-        }
-//        BufferedWriter bufferedWriter = null;
-//        FileWriter fileWriter = null;
-//        WriteFiles writeFiles = new WriteFiles();
-        // String data = delegatedUserIdList.toString() + " || " + delegationTimeList.toString() + " || " + chainList.toString() + " || latest file = " + fileName;// + " " + delegationTimeList.toString() + " " + chainList.toString();
-//        String data = theTour + ". " + delegatedUserIdList.toString() + " || " +
-//                userList.get(userIndex).getUserActivites().get(theTimestampIndex).getCurrentTimestamp() + " || " +
-//                chainList.toString() + " || " + " || latest file = " + fileName;
 
-//        tb.addRow(String.valueOf(theTour),
-//                delegatedUserIdList.toString(),
-//                String.valueOf(userList.get(userIndex).getUserActivites().get(theTimestampIndex).getCurrentTimestamp()),
-//                chainList.toString(),
-//                fileName);
-        tb.addRow(String.valueOf(theTour),
-                delegatedUserIdList.toString(),
-                String.valueOf(userList.get(userIndex).getUserActivites().get(theTimestampIndex).getCurrentTimestamp()),
-                chainList.toString());
-        //File file = new File(dirPath + "\\" + "Simulation_" + userList.get(userIndex).getUserId() + ".txt");
-//        File file = new File(dirPath + "\\" + friendUserID + "_Simulation" + ".txt");
-//        writeFile(file, data);
-        //writeFile(file, tb.toString());
-        /*
-         * DD/MM/yyyy hh:mm format
-         * */
-//        File chainFlow = new File(dirPath + "\\" + "ChainFlow_" + userList.get(userIndex).getUserId() + ".txt");
-        /*File chainFlow = new File(dirPath + "\\" + friendUserID + "_ChainFlow" + ".txt");
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY hh:mm:ss");
-        String dateString = sdf.format(userList.get(userIndex).getUserActivites().get(theTimestampIndex).getCurrentTimestamp());
-//        String chainData = theTour + ". " + userList.get(userIndex).getUserActivites().get(theTimestampIndex).getCurrentTimestamp()
-//                + " || " + chainList.toString();
-        tb = new TableBuilder();
-        tb.addRow(String.valueOf(theTour), dateString, String.valueOf(userList.get(userIndex).getUserActivites().get(theTimestampIndex).getOnlineFriendsList().size()), chainList.toString());
-
-        String chainData = theTour + ". " + dateString + " || " + chainList.toString();
-        writeFile(chainFlow, tb.toString());*/
-    }
-
-    public void deleteFile(String filePath) {
-        File file = new File(filePath);
-        if (file.exists()) {
-            try {
-                Files.deleteIfExists(file.toPath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void writeAllResult(ArrayList<DelegationInfo> delegationInfosArrayList, int indexOfUser, long friendUserID, long mainUserId, String delegationType) {
+    public void writeAllResult(ArrayList<DelegationInfo> delegationInfosArrayList, int indexOfUser,
+                               long friendUserID, long mainUserId, String delegationType,
+                               int onlineFriendsCount) {
         File dir;
         String dirPath = Constants.getOutputFolderPath() + "\\" + delegationType + "\\" + mainUserId;
         dir = new File(dirPath);
@@ -124,10 +64,11 @@ public class WriteFiles {
                 .trim();
 
         TableBuilder tb = new TableBuilder();
-/*
-        tb.addRow(" UserId", " Time Interval of FriendID", " Offline Days", " Hours", " Minutes"
-                , " TotalOfflineCount", " LCD", " Interrupted Session Count"
-                , " interruption total time", " Total Off Minutes", "Chain Length and Durations", "\n");*/
+
+        tb.addRow(" UserId", " Time Interval of FriendID", " Days", " Hours", " Minutes"
+                , " OfflineCount", " LCD", " Interrupted Count"
+                , " interruption total time", " Total Off Minutes",
+                "Total OF Count" , "Chain Length and Durations", "\n");
 //        String line = "UserId = " + delegationInfosArrayList.get(indexOfUser).getUserId() +
 //                "; Offline Days = " + days + " Hours = " + hours + " Minutes = " + minutes +
 //                "\n TotalOfflineCount = " + delegationInfosArrayList.get(indexOfUser).getTotalOfflineCount() +
@@ -146,6 +87,7 @@ public class WriteFiles {
                 , String.valueOf(delegationInfosArrayList.get(indexOfUser).getInterruptedSessionCount())
                 , String.valueOf(delegationInfosArrayList.get(indexOfUser).getInterruptTime())
                 , String.valueOf(delegationInfosArrayList.get(indexOfUser).getTotalOfflineTime())
+                , String.valueOf(onlineFriendsCount)
                 , chainInfosFormatted);
         writeFile(file, tb.toString());
     }
@@ -171,6 +113,8 @@ public class WriteFiles {
                 .trim();
         writeFile(chainFlow, formattedString);
 
+
+        /* detailed log
         File dirLog;
         String dirLogPath = dirPath + "\\DetailedLogs";
         dirLog = new File(dirLogPath);
@@ -188,11 +132,22 @@ public class WriteFiles {
                 .replace("[", "")  //remove the right bracket
                 .replace("]", "")  //remove the left bracket
                 .trim();
-        writeFile(detailedSimulationLog, formattedDetailString);
+        writeFile(detailedSimulationLog, formattedDetailString);*/
 //        writeFile(chainFlow, tableBuilder.toString());
     }
 
-
+public void createFolder(String path){
+    File dir;
+    String dirPath = Constants.getOutputFolderPath() + "\\" + path + "\\";
+    dir = new File(dirPath);
+    if (!dir.exists()) {
+        if (dir.mkdir()) {
+            System.out.println("Directory is created! " + path);
+        } else {
+            System.out.println("Failed to create directory! " + path);
+        }
+    }
+}
     public void writeFile(File file, String line) {
         BufferedWriter bufferedWriter = null;
         FileWriter fileWriter = null;
