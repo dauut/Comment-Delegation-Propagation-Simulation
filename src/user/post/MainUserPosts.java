254a2c4b9929cc0c735bac1c8f58731da051b479
Davut Uçar <davutucar@gmail.com>
2018-05-11 20:56:55 +0300

    Add files via upload

--- src/user/post/MainUserPosts.java
+++ src/user/post/MainUserPosts.java
@@ -1,33 +1,13 @@
 package user.post;
 
+import javafx.geometry.Pos;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainUserPosts {
-    private int mainUserPostCount;
     private int numOfPost;
-    private String postType;
-    private int postID;
-    private Date postCreateDate;
-    private int likeCount;
-    private int commentCount;
-    private int commentCharCount;
-    private int postSize;
-
-    public int getPostSize() {
-        return postSize;
-    }
-
-    public void setPostSize(int postSize) {
-        this.postSize = postSize;
-    }
-
-    public int getMainUserPostCount() {
-        return mainUserPostCount;
-    }
-
-    public void setMainUserPostCount(int mainUserPostCount) {
-        this.mainUserPostCount = mainUserPostCount;
-    }
+    private ArrayList<Posts> mainUserPostList;
 
     public int getNumOfPost() {
         return numOfPost;
@@ -37,51 +17,11 @@ public class MainUserPosts {
         this.numOfPost = numOfPost;
     }
 
-    public String getPostType() {
-        return postType;
+    public ArrayList<Posts> getMainUserPostList() {
+        return mainUserPostList;
     }
 
-    public void setPostType(String postType) {
-        this.postType = postType;
-    }
-
-    public int getPostID() {
-        return postID;
-    }
-
-    public void setPostID(int postID) {
-        this.postID = postID;
-    }
-
-    public Date getPostCreateDate() {
-        return postCreateDate;
-    }
-
-    public void setPostCreateDate(Date postCreateDate) {
-        this.postCreateDate = postCreateDate;
-    }
-
-    public int getLikeCount() {
-        return likeCount;
-    }
-
-    public void setLikeCount(int likeCount) {
-        this.likeCount = likeCount;
-    }
-
-    public int getCommentCount() {
-        return commentCount;
-    }
-
-    public void setCommentCount(int commentCount) {
-        this.commentCount = commentCount;
-    }
-
-    public int getCommentCharCount() {
-        return commentCharCount;
-    }
-
-    public void setCommentCharCount(int commentCharCount) {
-        this.commentCharCount = commentCharCount;
+    public void setMainUserPostList(ArrayList<Posts> mainUserPostList) {
+        this.mainUserPostList = mainUserPostList;
     }
 }
