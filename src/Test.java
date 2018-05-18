57960ecf461a261e00504249bd4364af813ca2c3
dauut <davutucar@gmail.com>
2018-05-18 10:27:46 +0300

    ideal case optimized

--- src/Test.java
+++ src/Test.java
@@ -1,49 +1,68 @@
-import constants.Constants;
-import io.TableBuilder;
-import io.WriteFiles;
-
 import java.io.File;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Test {
     public static void main(String[] args) {
-//        String[] mukofile = {"C:\\Users\\dauut\\Desktop\\data\\articles_100.train", "C:\\Users\\dauut\\Desktop\\data\\articles_1000.train",
-//                "C:\\Users\\dauut\\Desktop\\data\\articles_2500.train", "C:\\Users\\dauut\\Desktop\\data\\articles_10000.train"};
-//        File file;
-//        WriteFiles writeFiles = new WriteFiles();
-//        File newFile;
-//
-//        for (int i = 0; i < mukofile.length; i++) {
-//            file = new File(mukofile[i]);
-//            String line;
-//            try {
-//                Scanner scanner = new Scanner(file);
-//                int j = 0;
-//                while (scanner.hasNext()) {
-////                    lines.add(scanner.nextLine());
-//                    newFile = new File("C:\\Users\\dauut\\Desktop\\textout\\"+"text_" + i + "_" + j + ".txt");
-//                    line = scanner.nextLine();
-//                    line = line.substring(5,line.length());
-//                    writeFiles.writeFile(newFile,line);
-//                    j++;
-//                }
-//                scanner.close();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
+        String path = "C:\\Users\\dauut\\Desktop\\12813704_Simulation_general_info.txt";
 
-        int x =0;
+        File file = new File(path);
+        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<String> users = new ArrayList<>();
+        ArrayList<String> filesPath = new ArrayList<>();
+        ArrayList<String> files = new ArrayList<>();
 
-        for (int i = 0; i < x; i++){
-            System.out.println("Davut");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                lines.add(scanner.nextLine());
+            }
+
+            String delims = "[ ]";
+            String delims1 = "\t";
+            for (int i = 0; i < lines.size(); i++) {
+                String[] firstLineToken = lines.get(i).split(delims);
+                String[] inFirs = firstLineToken[0].split(delims1);
+                users.add(inFirs[2]);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
-    }
+        File results = new File("C:\\Users\\dauut\\Desktop\\sub");
+        File[] listofFiles = results.listFiles();
 
+        String delims2 = "_";
+        String[] token;
+        for (int i = 0; i < listofFiles.length; i++) {
+            token = listofFiles[i].getName().split(delims2);
+            files.add(token[0]);
+            filesPath.add(listofFiles[i].getAbsolutePath());
+        }
+
+        for (int i = 0; i < users.size(); i++) {
+            int counter = 0;
+            boolean found = false;
+            while (!found && counter < files.size()) {
+                if (!users.get(i).equals(files.get(counter))) {
+                    counter++;
+                }else{
+                    found = true;
+                }
+            }
+            if (counter < files.size()) {
+                File deleteFile = new File(filesPath.get(counter));
+                if (deleteFile.delete()) {
+                    System.out.println(filesPath.get(counter) + "File deleted successfully");
+                } else {
+                    System.out.println(files.get(counter) + "Failed to delete the file");
+                }
+            }
+        }
+
+
+    }
 }
